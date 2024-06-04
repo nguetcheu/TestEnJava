b91a6ab03c501312c6ee6796208307654de12e6b
nguetcheu <dnguetcheu@gmail.com>
2024-06-04 17:59:15 +0200

    Ecriture du test unitaire pour la multiplication

--- src/test/java/com/openclassrooms/testing/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatorTest.java
@@ -8,7 +8,7 @@ public class CalculatorTest {
 
     @Test
     void testAddTwoPositiveNumbers() {
-        // arrange
+        // Arrange
         int a = 2;
         int b = 3;
         Calculator calculator = new Calculator();
@@ -19,5 +19,18 @@ public class CalculatorTest {
         // Assert
         assertEquals(5, somme);
     }
-    
+
+    @Test
+    void multiplyTwoPositiveNumbers() {
+        // Arrange
+        int a = 3;
+        int b = 5;
+        Calculator calculator = new Calculator();
+
+        // Act
+        int multplication = calculator.multiply(a,b);
+
+        // Assert
+        assertEquals(15, multplication);
+    }
 }
